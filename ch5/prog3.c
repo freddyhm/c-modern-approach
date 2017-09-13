2b6530ec3f01de9a0da71b9b54fbfc10ae6939f0
Freddy Hidalgo-Monchez <me@freddyhm.com>
2017-09-12 21:11:54 -0400

    add prog3

---
+++ ch5/prog3.c
@@ -0,0 +1,23 @@
+/*
+ * Author: Freddy Hidalgo-Monchez
+ * Purpose: Calculates a competitor broker's commission
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int shares = 0;
+	float price = 0.0f;
+
+	// prompt user for number of shares and price per share
+	printf("How many shares did you purchase?: \n");
+	scanf("%d", &shares);
+
+	printf("How much was the price per share?: \n");
+	scanf("%d", &price);
+
+	// calculate a competitor's fee
+
+
+}
