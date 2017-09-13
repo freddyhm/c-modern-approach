e060c57eddd52d01e1e8f71f47dbc15ceb7e4b35
Freddy Hidalgo-Monchez <me@freddyhm.com>
2017-09-12 20:35:53 -0400

    finish prog1

---
+++ ch5/prog1.c
@@ -0,0 +1,28 @@
+/*
+ * Author: Freddy Hidalgo-Monchez
+ * Purpose: Calculaes how many digits a number contains
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 0;
+
+	// prompt user for a number
+	printf("Enter a number: ");
+	scanf("%d", &num);
+
+	// determine if it is 1 or more digits assuming number is bigger than 0
+	if(num <= 9){
+		printf("The number has 1 digit\n");    
+	}else if(num <= 99){
+		printf("The number has 2 digits\n");
+	}else if(num <= 999){
+		printf("The number has 3 digits\n");
+	}else{
+		printf("The number has 4 digits\n");
+	}
+
+	return 0;
+}
