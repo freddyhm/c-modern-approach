40653d75c7ac8a453d6569e97d71720ddd3a7aa2
Freddy Hidalgo-Monchez <me@freddyhm.com>
2017-09-13 20:28:02 -0400

    add prog3

--- ch5/prog3.c
+++ ch5/prog3.c
@@ -8,16 +8,21 @@
 int main(void)
 {
 	int shares = 0;
+	float rival_base = 33.0f;
 	float price = 0.0f;
+	float rival_fee = 0.0f;
 
 	// prompt user for number of shares and price per share
-	printf("How many shares did you purchase?: \n");
+	printf("How many shares did you purchase?: ");
 	scanf("%d", &shares);
 
-	printf("How much was the price per share?: \n");
-	scanf("%d", &price);
+	printf("How much was the price per share?: ");
+	scanf("%f", &price);	
 
-	// calculate a competitor's fee
+	// calculate competitor's fee
+	float scale = shares < 2000 ? (0.03f * shares) : (0.02f * shares);
+	rival_fee = rival_base + scale; 
 
-
+	printf("Commission from competitor broker: $%.2f\n", rival_fee);
+	return 0;
 }
